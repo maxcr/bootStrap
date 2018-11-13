e35c7937ce9f232caf4a6c5f631e52d10a4f0426
Max C. Robbins <max.conrad.robbins@gmail.com>
2018-11-13 09:27:30 -0700

    first commit

---
+++ bsArch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+timedatectl set-ntp true
+curl -O https://static.maxrobbins.com/idealFormat.sfdisk
+printf "$(lsblk -o size,name -nl)"
+printf "\n\nWhat device would you like to use? \n
+DRIVE ONLY AKA sda,sdb,sdc NO PART NUMBER!! \n"
+read -p "Device Name:" devName
+read -p "You entered ${devName} is this correct?" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
